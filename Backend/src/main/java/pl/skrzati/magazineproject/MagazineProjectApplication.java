f3dbc70c79d050977320c9cef0bca1248155f4af
Mateusz Janusz <mateuszjanusz@MacBook-Air-Mateusz.local>
2026-04-01 22:51:46 +0200

    Initial Commit

---
+++ Backend/src/main/java/pl/skrzati/magazineproject/MagazineProjectApplication.java
@@ -0,0 +1,13 @@
+package pl.skrzati.magazineproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagazineProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MagazineProjectApplication.class, args);
+    }
+
+}
---
+++ Backend/src/test/java/pl/skrzati/magazineproject/MagazineProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.skrzati.magazineproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagazineProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
